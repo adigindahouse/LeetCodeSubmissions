8196001bfdebd4cc1d4ac5a6b97d6889b9b59d4a
adigindahouse <adigindahouse>
2015-02-22 11:06:24 -0600

    More submissions

---
+++ IsBinaryTreeBalanced.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+ 
+class RetClass{
+    int depth;
+    boolean isBalanced;
+    
+    RetClass(boolean isBalanced, int depth){
+        this.isBalanced = isBalanced;
+        this.depth = depth;
+    }
+}
+
+public class IsBinaryTreeBalanced {
+    public boolean isBalanced(TreeNode root) {
+        RetClass ret = isBalancedModified(root);
+        return ret.isBalanced;
+    }
+    
+    public RetClass isBalancedModified(TreeNode root){
+        //Base case: if root is null - the the tree is trivially balanced
+        if(root==null){
+            return new RetClass(true, 0);
+        }
+        
+        //Get the height of the left subtree h1 and if it is balanced - say b1
+        RetClass leftDetails = isBalancedModified(root.left);
+        
+        //if(!b1){ return false, <dummy height>; }
+        if(!leftDetails.isBalanced){
+            return new RetClass(false, -1);
+        }
+        
+        //Get the height of the left subtree h2 and if it is balanced - say b2
+        RetClass rightDetails = isBalancedModified(root.right);
+
+        //if(!b2){ return false, <dummy height>; }
+        if(!rightDetails.isBalanced){
+            return new RetClass(false, -1);
+        }
+        
+        //b1 && b2
+        //return (abs(h1-h2)<=1, max(h1, h2) + 1)
+        return new RetClass(Math.abs(leftDetails.depth - rightDetails.depth)<=1, (int)Math.max(leftDetails.depth, rightDetails.depth)+1);
+        
+    }
+}
---
+++ MajorityElement.java
@@ -0,0 +1,40 @@
+public class MajorityElement {
+    public int majorityElement(int[] num) {
+        return findCand(num);
+    }
+    
+    //Return the the majority element
+    public int findCand(int[] num){
+        int majorityIndex = 0, count = 1;
+        
+        for(int i=1;i<num.length;i++){
+            if(num[i] == num[majorityIndex]){
+                count++;
+            } else {
+                count--;
+            }
+            
+            if(count==0){
+                majorityIndex = i;
+                count = 1;
+            }
+        }
+        
+        return num[majorityIndex];
+    }
+    
+    public boolean isMajority(int[] num, int cand){
+        int count = 0 ;
+        
+        for(int i:num){
+            if(i==cand){
+                count++;
+            }
+        }
+        
+        if(count>num.length/2){
+            return true;
+        } 
+        return false;
+    }
+}
